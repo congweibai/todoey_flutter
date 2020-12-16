7372c58a7249cc81141496da23efa88647bf83ec
Congwei Bai <congweibai@gmail.com>
2020-12-16 22:54:02 +1030

    layout finish

---
+++ lib/widgets/task_tile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = true;
+
+  void checkboxCallback(bool newValue) {
+    setState(() {
+      isChecked = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: TaskCheckbox(isChecked, checkboxCallback),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+
+  TaskCheckbox(this.checkboxState, this.toggleCheckboxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxState,
+      activeColor: Colors.lightBlueAccent,
+      onChanged: (newValue) {
+        toggleCheckboxState(newValue);
+      },
+    );
+  }
+}
